c46bb862f83c85268bd5d098eeb42bd51e992fce
iker0610 <ikerprogrammer0610@gmail.com>
2022-04-06 19:47:35 +0200

    Entry Points programmed

--- api/app/crud.py
+++ api/app/crud.py
@@ -1,18 +1,18 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import entities, api_models
 
 
-def get_user(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
+def get_user(db: Session, username: str) -> entities.User:
+    return db.query(entities.User).filter(entities.User.username == username).first()
 
 
-def get_user_password(db: Session, username: str):
-    return db.query(models.User.hashed_password).filter(models.User.username == username).first()
+def get_user_password(db: Session, username: str) -> str:
+    return db.query(entities.User.hashed_password).filter(entities.User.username == username).first()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(username=user.username, hashed_password=user.hashed_password)
+def create_user(db: Session, user: api_models.UserCreate):
+    db_user = entities.User(username=user.username, hashed_password=user.hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
-from . import crud, models, schemas
-from .database import SessionLocal, engine
 
-models.Base.metadata.create_all(bind=engine)
+from . import crud, api_models
+from .database import SessionLocal
+
+# entities.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -27,6 +29,17 @@ async def root():
     return RedirectResponse(url='/docs')
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/users/", response_model=api_models.User, status_code=status.HTTP_201_CREATED)
+def create_user(user: api_models.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_password(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Username already registered.")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/{username}", response_model=api_models.User, status_code=status.HTTP_200_OK)
+def read_user(username: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, username=username)
+    if db_user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return db_user
